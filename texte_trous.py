ebc7807d54b04688fc059b9fc6e3722c65cc492f
msw9 <103287262+msw9@users.noreply.github.com>
2024-02-23 22:35:59 +0100

    gestion des exceptions

--- texte_trous.py
+++ texte_trous.py
@@ -10,7 +10,7 @@ générer des cartes anki texte à trou
 # variables
 deck = {}
 nlp = spacy.load('ja_core_news_sm',disable=['ner','tagger'])
-particles = {'から','まで','までに','より','ほど','くらい','ぐらい','ころ','ごろ','ばかり'}
+particles = {'なんて','なんか'}
 max_length = 16383
 tagger = Tagger()
 
@@ -24,18 +24,27 @@ def answer(texte, particles):
     for particle in particles:
         texte = re.sub(particle, f"<font color=\"#0000ff\">{particle}</font>", texte)
     return texte
+
+def make_quizz(phrases): #list
+    deck = {}
+    deck['question'] = [question(phrase, particles) for phrase in phrases]
+    deck['answer'] = [answer(phrase, particles) for phrase in phrases]
+    df = pd.DataFrame.from_dict(deck)
+    df.to_csv('quizz_jp.csv', index=False, header=False,sep='\t')
 #main
 with open ('ikebukuro.txt', encoding='utf-8') as f:
     livre = f.read()
 
 chunks = [livre[i:i+max_length] for i in range(0,len(livre),max_length)]
 doc = nlp(random.choice(chunks))
-# phrases = [sent.text for sent in doc.sents if any(particle in sent.text for particle in particles)]
 phrases = [sent.text for sent in doc.sents]
 phrases = [phrase for phrase in phrases if any(particle in [word.surface for word in tagger(phrase)] for particle in particles)]
-dix_phrases = random.sample(phrases, 10)
-deck['question'] = [question(phrase, particles) for phrase in dix_phrases]
-deck['answer'] = [answer(phrase, particles) for phrase in dix_phrases]
 
-df = pd.DataFrame.from_dict(deck)
-df.to_csv('quizz_jp.csv', index=False, header=False,sep='\t')
+try:
+    dix_phrases = random.sample(phrases,10)
+    make_quizz(dix_phrases)
+except:
+    if phrases == []:
+        print("Il n'y a pas de phrases contenant cette sous-chaîne de caractères")
+    else:
+        make_quizz(phrases) # s'il y a moins de dix phrases contenant la sous-chaîne
