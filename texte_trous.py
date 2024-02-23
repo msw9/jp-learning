54b65098edadf2bb6c35d488e96bb9a95d395425
msw9 <103287262+msw9@users.noreply.github.com>
2024-02-23 21:44:44 +0100

    fixed + exceptions
    ajout d'exceptions si texte ne contient pas la sous-chaîne ou contient moins de 10 itérations de la sous-chaine

--- texte_trous.py
+++ texte_trous.py
@@ -9,7 +9,7 @@ version alt pour mongues expressions et contexte
 # variables
 deck = {}
 nlp = spacy.load('ja_core_news_sm',disable=['ner','tagger'])
-particles = {'わけがない','はずがない'}
+particles = {'でしょうか','のですか','んです'}
 max_length = 16383
 
 #fonctions
@@ -22,28 +22,35 @@ def answer(texte, particles):
     for particle in particles:
         texte = re.sub(particle, f"<font color=\"#0000ff\">{particle}</font>", texte)
     return texte
+
+def make_quizz(phrases): #list
+    deck = {}
+    deck['question'] = [question(phrase, particles) for phrase in phrases]
+    deck['answer'] = [answer(phrase, particles) for phrase in phrases]
+    df = pd.DataFrame.from_dict(deck)
+    df.to_csv('quizz_jp.csv', index=False, header=False,sep='\t')
+    
 #main
-with open ('ikebukuro.txt', encoding='utf-8') as f:
+with open ('pride.txt', encoding='utf-8') as f:
     livre = f.read()
 
 chunks = [livre[i:i+max_length] for i in range(0,len(livre),max_length)]
 doc = nlp(random.choice(chunks))
-# phrases = [sent.text for sent in doc.sents if any(particle in sent.text for particle in particles)]
 phrases = []
 sentences = list(doc.sents)
+sentences = [sent.text for sent in sentences]
 for i, sent in enumerate(sentences):
     phrase = ""
-    if any(particle in sent.text for particle in particles):
+    if any(particle in sent for particle in particles):
         if i > 0:
-            phrase += doc.sents[i-1].text
-        phrase += sent.text
+            phrase += sentences[i-1]
+        phrase += sent
         phrases.append(phrase)
-print(phrases)
-"""
-dix_phrases = random.sample(phrases,10)
-deck['question'] = [question(phrase, particles) for phrase in dix_phrases]
-deck['answer'] = [answer(phrase, particles) for phrase in dix_phrases]
-
-df = pd.DataFrame.from_dict(deck)
-df.to_csv('quizz_jp.csv', index=False, header=False,sep='\t')
-"""
+try:
+    dix_phrases = random.sample(phrases,10)
+    make_quizz(dix_phrases)
+except:
+    if phrases == []:
+        print("Il n'y a pas de phrases contenant cette sous-chaîne de caractères")
+    else:
+        make_quizz(phrases) # s'il y a moins de dix phrases contenant la sous-chaîne
