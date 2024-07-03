0505ead3387b90436e1d7ee5735aae9e3645c53d
msw9 <103287262+msw9@users.noreply.github.com>
2024-07-03 14:53:49 +0200

    vers Bernouilli

--- classification/text_classification.py
+++ classification/text_classification.py
@@ -28,8 +28,26 @@ def vocabulary(tagger,data): # YES
             if word.surface not in memo:
                 memo.append(word.surface)
     return len(memo)
-def p_t_c_bernouilli():
-    yield ()/(+ v)
+
+def p_t_c_bernouilli(phrase,c,data,tagger,v):
+    data = list(filter(lambda x:x[1]==c,data))
+    for word in tagger(phrase):
+        n_c_t = len(list(filter(lambda x:word.surface in x[0],data)))
+        yield (n_c_t + 1)/ (data + v)
+        
+def classify(mode,phrase,i,train,tagger,v,n):
+    if mode == 'bayes':
+        liste = [(nbr_of_c(train,i)/n) * np.prod(list(p_t_c(phrase,i,train,tagger,v))) for i in range(1,6)]
+    elif mode == 'bernouilli':
+        liste = [(nbr_of_c(train,i)/n) * np.prod(list(p_t_c_bernouilli(phrase,i,train,tagger,v))) for i in range(1,6)]
+    index = 0
+    max_element = liste[0]
+    for i in range(1,len(liste)):
+        if liste[i] > max_element:
+            max_element = liste[i]
+            index = i
+    return index + 1
+    
 def main_naive_bayes(corpus):#df
     tagger = Tagger()
     train_df = corpus.sample(frac=0.8,random_state=200)
@@ -47,17 +65,10 @@ def main_naive_bayes(corpus):#df
     """
      # TEST 
     test = test_df.values.tolist()
-    phrase = "オスマンの大規模開発事業を抗議した。"
-    liste = [(nbr_of_c(train,i)/n) * np.prod(list(p_t_c(phrase,i,train,tagger,v))) for i in range(1,6)]
-    index = 0
-    max_element = liste[0]
-    for i in range(1,len(liste)):
-        if liste[i] > max_element:
-            max_element = liste[i]
-            index = i
-    print(liste)
-    print(max_element, index + 1)
-    
+    for entry in test:
+        for i in range (1,6):
+            print(classify('bayes',entry[0],i,train,tagger,v,n))
+            
 def main_bernouilli(corpus):
     tagger = Tagger()
     train_df = corpus.sample(frac=0.8,random_state=200)
@@ -66,8 +77,11 @@ def main_bernouilli(corpus):
     train = train_df.values.tolist()
     n = len(train)
     v = 5 # car 5 niveaux JLPT
-    
+    for entry in test:
+        for i in range (1,6):
+            print(classify('bernouilli',entry[0],i,train,tagger,v,n))
+                  
 if __name__=='__main__':
     df = pd.read_csv('corpus.csv')
     main_naive_bayes(df)
-    main_bernouilli(data)
+    main_bernouilli(df)
